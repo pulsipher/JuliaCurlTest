db180dc3182de639865d2bc3f6a4e2c73073b4d5
pulsipher <pulsipher@wisc.edu>
2021-06-11 17:59:28 -0500

    update to Ipopt only

--- curl.jl
+++ curl.jl
@@ -1,2 +1,4 @@
+using Pkg; Pkg.add("Ipopt"); using Iopot
+
 null_file = @static Sys.iswindows() ? "nul" : "/dev/null"
-run(`curl -sI --proto =http,https,ftp,ftps https://google.com --max-time 10 -o $(null_file) --write-out '%{http_code} %{url_effective} %{redirect_url}'`)
+run(`curl -sI --proto =http,https,ftp,ftps https://google.com/ --max-time 10 -o $(null_file) --write-out '%{http_code} %{url_effective} %{redirect_url}'`)
--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,4 @@
-using Documenter, InfiniteOpt
+using Documenter, Ipopt
 
 # Make the documentation via Documenter.jl
 makedocs(;
