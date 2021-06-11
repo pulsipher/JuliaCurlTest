b75398755f2243f85288e29f7300aa9497140b28
pulsipher <pulsipher@wisc.edu>
2021-06-11 17:15:57 -0500

    google site

--- curl.jl
+++ curl.jl
@@ -1,2 +1,2 @@
 null_file = @static Sys.iswindows() ? "nul" : "/dev/null"
-run(`curl -sI --proto =http,https,ftp,ftps https://jump.dev/JuMP.jl/v0.21.8/ --max-time 10 -o $(null_file) --write-out '%{http_code} %{url_effective} %{redirect_url}'`)
+run(`curl -sI --proto =http,https,ftp,ftps https://google.com --max-time 10 -o $(null_file) --write-out '%{http_code} %{url_effective} %{redirect_url}'`)
