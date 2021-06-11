c4c41c11056ebcb5b1fa69e7785ed3e49d5faed2
pulsipher <pulsipher@wisc.edu>
2021-06-11 18:01:54 -0500

    typo fix

--- curl.jl
+++ curl.jl
@@ -1,4 +1,4 @@
-using Pkg; Pkg.add("Ipopt"); using Iopot
+using Pkg; Pkg.add("Ipopt"); using Ipopt
 
 null_file = @static Sys.iswindows() ? "nul" : "/dev/null"
 run(`curl -sI --proto =http,https,ftp,ftps https://google.com/ --max-time 10 -o $(null_file) --write-out '%{http_code} %{url_effective} %{redirect_url}'`)
